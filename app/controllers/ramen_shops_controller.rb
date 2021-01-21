f0542f36edfcd52839c9c13b2b5a0c4835208553
hayahide <copernicus0219@gmail.com>
2021-01-21 11:43:34 +0000

    add ramen_review

---
+++ app/controllers/ramen_shops_controller.rb
@@ -0,0 +1,13 @@
+class RamenShopsController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+  
+  def index
+    @ramen_shops = RamenShop.all
+  end
+
+  def show
+    @ramen_shop = RamenShop.find(params[:id])
+    @review = Review.new
+  end
+  
+end
